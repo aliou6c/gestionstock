0a8406571214af0ecae691a7df74dc431af84040
aliou cisse <aliou6c@gmail.com>
2018-10-30 21:35:03 +0000

    remove passwd et username

--- gestionstock-utils/src/main/java/sn/objis/gestionstock/utils/MySqlConnection.java
+++ gestionstock-utils/src/main/java/sn/objis/gestionstock/utils/MySqlConnection.java
@@ -18,8 +18,8 @@ import java.util.logging.Logger;
 public class MySqlConnection {
 	
 	private static String url="jdbc:mysql://localhost/bd_gestionstock?useSSL=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	private static String userDb= "alioucisse";
-	private static String pwdDb= "alioucisse";
+	private static String userDb;
+	private static String pwdDb;
 	private static Connection conn= null;//l'unique instance de connexion à la base
 	
 	//Constructeur priv� pour blocquer la cr�ation d'instance de la classe
